bca0f499e3d6259578951509b71f7b0c2d62ca7c
Jose Carlos <j.ccarlos.j1989@gmail.com>
2021-08-31 10:27:38 -0300

    Add configurations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'educations/show'
   devise_for :users
   root to: 'pages#home'
+
+  resources :educations, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
