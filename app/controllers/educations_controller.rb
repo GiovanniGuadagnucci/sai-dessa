ebff7b17bc26e0973f0e03baf8ab0b8350149411
Victor Moreira <victorsmoreira@outlook.com>
2021-09-03 14:17:55 -0300

    front da página de vídeos educativos

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -2,7 +2,7 @@ class EducationsController < ApplicationController
   def index
     @educations = Education.where(category: params[:fase].present? ? "#{params[:fase]}_intro" : current_user.undone_categories)
     if params[:fase].present?
-      @next_path = { text: "Boa, bora para o teste!",
+      @next_path = { text: "Boa, bora pro teste!",
                      path: questoes_path }
     else
       @next_path = { text: "Assisti os conteudos!",
