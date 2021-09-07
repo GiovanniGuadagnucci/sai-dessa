ff1db252ba4019f0600251b8fb87e2c5ff21b256
giovanni guadagnucci <giovanniguadag@gmail.com>
2021-09-07 01:17:43 -0300

    Questions Home - Falta o Js no Botão click

--- app/javascript/components/init_choose_answer.js
+++ app/javascript/components/init_choose_answer.js
@@ -1,21 +1,24 @@
 const initChooseAnswer = () => {
-  const questions = document.querySelectorAll(".options");
+  const questions = document.querySelectorAll(".question-choices");
   if (questions) {
-    questions.forEach(options => {
-      const answers = options.querySelectorAll('.option');
-      answers.forEach(answer => {
+    questions.forEach((options) => {
+      const answers = options.querySelectorAll(".option");
+      answers.forEach((answer) => {
         answer.addEventListener("click", () => {
-          const choosen = options.querySelector('.choosen-option');
+          var style = window.getComputedStyle(answer);
+          console.log(style.getPropertyValue("background-color"));
+          const choosen = options.querySelector(".choosen-option");
           if (choosen) {
+            choosen.style.background-color = color.opacity();
             choosen.classList.remove("choosen-option");
             answer.classList.add("choosen-option");
           } else {
             answer.classList.add("choosen-option");
           }
-        })
+        });
       });
     });
   }
-}
+};
 
 export { initChooseAnswer };
