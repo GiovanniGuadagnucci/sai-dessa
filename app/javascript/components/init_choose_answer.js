60606e30bca145ef0e5d57f41be3aad8c294b485
giovanni guadagnucci <giovanniguadag@gmail.com>
2021-09-07 10:38:47 -0300

    Js buttons colors (valeu neto!)

--- app/javascript/components/init_choose_answer.js
+++ app/javascript/components/init_choose_answer.js
@@ -5,13 +5,17 @@ const initChooseAnswer = () => {
       const answers = options.querySelectorAll(".option");
       answers.forEach((answer) => {
         answer.addEventListener("click", () => {
-          var style = window.getComputedStyle(answer);
-          console.log(style.getPropertyValue("background-color"));
+          const color = answer.dataset.color;
           const choosen = options.querySelector(".choosen-option");
           if (choosen) {
             choosen.classList.remove("choosen-option");
+            choosen.classList.remove("background-green");
+            choosen.classList.remove("background-gray");
+            choosen.classList.remove("background-brown");
             answer.classList.add("choosen-option");
+            answer.classList.add(`${color}`);
           } else {
+            answer.classList.add(`${color}`);
             answer.classList.add("choosen-option");
           }
         });
