dafdab35f246a4f996a1d172b8b91f9893a3e2b2
Victor Moreira <victorsmoreira@outlook.com>
2021-09-07 18:59:30 -0300

    mudei tudo

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -3,11 +3,9 @@ class EducationsController < ApplicationController
     fase = current_user.current_phase
     @educations = Education.where(category: current_user.phase_not_started? ? "#{fase}_intro" : current_user.undone_categories )
     if current_user.phase_not_started?
-      @next_path = { text: "Boa, bora pro teste!",
-                     path: questoes_path }
+      @next_path = { text: "Boa, bora pro teste!", path: questoes_path }
     else
-      @next_path = { text: "Assisti os conteudos!",
-                     path: questoes_path({ oath: "oath" }) }
+      @next_path = { text: "Assisti os conteudos!", path: questoes_path }
     end
   end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,9 @@
 class QuestionsController < ApplicationController
   def index
     current_user.start_phase if current_user.phase_not_started?
+    @current_phase = current_user.current_phase
 
-    if params[:oath]
+    if current_user.user_oath_count == 1
       @questions = Question.where(category: "#{current_user.current_phase}_oath")
       @next_path = { text: "Eu Juro! Bora pro teste!", path: nextstep_path }
     else
@@ -15,29 +16,18 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
   end
 
+  private
+
   def nextstep
     if current_user.user_score >= SD["fourth_phase"]["score"]
       redirect_to jornada_final_path
     elsif current_user.undone_categories.empty?
       redirect_to fase_path
-    elsif current_user.user_oath_count.zero?
+    elsif current_user.user_oath_count == 1
       redirect_to educacao_path
-    elsif current_user.oath_score >= 80 && current_user.user_oath_count == 1
-      current_user.last_chance
+    elsif current_user.oath_score >= 80 && current_user.user_oath_count == 2
       redirect_to questoes_path
-    else
-      redirect_to fim_path
+    else redirect_to fim_path
     end
   end
-
-
-
-
-
-
-
-
-
-
-
 end
