55816ea1f742e9c95d1448327a4397df35e9db7c
giovanni guadagnucci <giovanniguadag@gmail.com>
2021-09-08 15:06:07 -0300

    Seed e ajuste na main!

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -2,6 +2,7 @@ class EducationsController < ApplicationController
   def index
     fase = current_user.current_phase
     @educations = Education.where(category: current_user.phase_not_started? ? "#{fase}_intro" : current_user.undone_categories )
+    @categories = @educations.map(&:category).uniq
     if current_user.phase_not_started?
       @next_path = { text: "Boa, bora pro teste!", path: questoes_path }
     else
--- config/initializers/sai_dessa_constansts.rb
+++ config/initializers/sai_dessa_constansts.rb
@@ -16,3 +16,37 @@ SD = {
     "score" => 14
   }
 }
+
+SD_DESCRIPTION = {
+  "first_phase" => {
+    "description" => "Nessa etapa queremos entender seu autoconhecimento, seus objetivos e valores pessoais! Está animado para o inicio da sua jornada?",
+    "categories" => { "self_knowledge" => "Posso afirmar com segurança que eu me conheço?",
+                      "personal_objectives" => "Tenho proposito de vida (o que me anima a levantar diariamente todos os dias ) bem defido em minha mente?",
+                      "personal_values" => "Tenho na ponta da lingua quais são meus valores pessoais?",
+                    }
+  },
+  "second_phase" => {
+    "description" => "Parabéns a maioria das pessoas que chegaram até aqui tambem chegaram no final! Hoje elas estão mais felizes profissionalmente.
+    Nessa etapa queremos entender suas hardskills e softskills, pontos fortes e pontos fracos e o que precisa ser desenvolvido",
+    "categories" => { "hard_skills" => "Conheço minhas habilidades técnicas?",
+                      "soft_skills" => "Conheço minhas soft skills?",
+                      "strong_points" => "Tenho na ponta da lingua as meus pontos fracos?",
+                      "week_points" => "Tenho na ponta da lingua as meus pontos fortes?",
+                    }
+  },
+  "third_phase" => {
+    "description" => "Mudando o foco! Agora que voce olhou para dentro queremos te ajudar a olhar para fora! Nessa etapa olharemos as empresas! Conheceremos suas culturas, valores, caracteristicas e quais cargos que voce pode se encaixar! Está pronto?!",
+    "categories" => { "company_culture" => "Qual a importancia da cultura empresarial? Como encontra-las?",
+                      "company_values" => "Sabe reconhecer os valores empresariais que se harmonizam aos seus?",
+                      "company_characteristics" => "Quais as caracteristicas empresariais te interessam?",
+                      "positions" => "Como acho o cargo dentro da empresa que será o meu?",
+                    }
+  },
+  "fourth_phase" => {
+    "description" => "Estamos quase no fim! Passou muito rápido, não é? Agora que voce sabe onde está e pra onde quer ir, queremos te ajudar a identicar o pontos que voce precisa desenvolver! Nessa fase entenderemos quais são as Hards e Soft skills que voce precisa desenvolver para a vaga que escolheu! Bora?",
+    "categories" => { "references" => "Existem pessoas que já estão lá?",
+                      "new_hard_skills" => "Sei o que tecnicamente tenho que desenvolver?",
+                      "new_soft_skills" => "Tenho todas as caracteristicas para o cargo?",
+                    }
+  }
+ }
